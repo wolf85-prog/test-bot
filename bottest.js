88def03ffe717a3b14a8b40c342e1a75fbff1f9a
Sergey <seregaland@yandex.ru>
2023-05-03 15:47:23 +0300

    init

---
+++ bottest.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+//telegram api
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+const https = require('https');
+
+const token = process.env.TELEGRAM_API_TOKEN
+
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Я принял ваш запрос!')
+})
+
+//-------------------------------------------------------------------------------------------------------------------------------
+const PORT = process.env.PORT;
+
+const start = async () => {
+    try {
+        
+        httpsServer.listen(PORT, () => {
+            console.log('HTTPS Server BotTest running on port ' + PORT);
+        });
+
+    } catch (error) {
+        console.log('Подключение к БД сломалось!', error)
+    }
+}
+
+start()
