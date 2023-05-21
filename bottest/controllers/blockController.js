4fe895efb291c02c41f69dfb51e580d71a567ec3
seregey gornih <you@example.com>
2023-05-21 07:57:55 +0300

    fix getBlocks

--- bottest/controllers/blockController.js
+++ bottest/controllers/blockController.js
@@ -10,16 +10,20 @@ async function getBlocks(blockId) {
         });
 
         let count = 0;
+        let res;
 
         const responseResults = response.results.map((block) => {
-            //if (block.child_database.title == "Основной состав" || block.child_database.title == "Назначенные")
-            if (block.child_database) {
-                count++;
+            if (block.child_database.title == "Основной состав"){
+               res = id 
             }
+            
+            // if (block.child_database) {
+            //     count++;
+            // }
         });
 
-        let res;
-        (count >1) ? res = response.results[1].id : res = response.results[0].id     
+        
+        //(count >1) ? res = response.results[1].id : res = response.results[0].id     
         
         console.log("Blocks Data: "  + res)
 
