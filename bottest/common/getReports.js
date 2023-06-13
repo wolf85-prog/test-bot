40c36bdc29e90ca93cf7f5f38dfba1482bb1fb2a
Sergey <seregaland@yandex.ru>
2023-06-13 14:15:17 +0300

    fix

--- bottest/common/getReports.js
+++ bottest/common/getReports.js
@@ -55,6 +55,7 @@ module.exports = async function getReports(project, bot) {
 
         //получить уникальные даты из Основного состава по возрастанию
         dates = [...allDate].filter((el, ind) => ind === allDate.indexOf(el));
+        console.log(dates)
         const sortedDates = [...dates].sort((a, b) => {       
             var dateA = new Date(a), dateB = new Date(b) 
             return dateA-dateB  //сортировка по возрастающей дате  
