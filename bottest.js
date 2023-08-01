56f74ded70ccbc3255c41ff93f0b826360f7f02b
seregey gornih <you@example.com>
2023-08-01 11:27:27 +0300

    fix

--- bottest.js
+++ bottest.js
@@ -103,13 +103,13 @@ bottest.on('message', async (msg) => {
 
         // startreports {id проекта}
         if(text.startsWith('/startposter')) {
-            //const poster = 'https://proj.uley.team:5000/images/pencil.png'
+            const poster = 'https://proj.uley.team/files/pre/2023-04-05T10:43:25.626Z.png'
             //const poster = 'AgACAgIAAxkBAAIE_mTH85gAAdFG-dnhCKYJOvGnoRPT4AACg84xG8bCQUqWwWWWheWdigEAAwIAA3kAAy8E'
             //const poster = '/admin/telegram-bot-amin-panel/server/images/pencil.png'
             //const poster = 'https://s1.1zoom.ru/big0/52/Love_Sunrises_and_sunsets_Fingers_Hands_Heart_Sun_532758_1280x897.jpg'
             //const poster = 'http://proj.uley.team:5151/files/pre/test6_test6_customer.pdf'
 
-            const poster = __dirname + '/../files/2023-04-15T11:00:06.499Z.pdf';
+            //const poster = __dirname + '/../files/pre/2023-04-05T10:43:25.626Z.png';
 
             // if (poster) {
             //     await bottest.sendDocument({
