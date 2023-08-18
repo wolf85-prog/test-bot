2e9e04bc10f36e6f2c7c0f700f4de8fb8dc63a97
seregey gornih <you@example.com>
2023-08-18 10:54:23 +0300

    fix

--- bottest/common/getReports.js
+++ bottest/common/getReports.js
@@ -246,9 +246,11 @@ ${arr_copy.map((item, index) =>'0' + (index+1) + '. '+ item.title + ' = ' + item
     
                             }, 40000) 
 
+                            console.log("tasks1: ", tasks1)
+
                             await Task.create(
                             {
-                                timer: JSON.stringify(tasks1), 
+                                timer: tasks1, 
                                 projectId: project.projectId, 
                             })
                         }        
