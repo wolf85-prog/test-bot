a119c5ada31a025a2bdae6f91d38e4c448d156ab
seregey gornih <you@example.com>
2023-08-22 17:19:29 +0300

    fix

--- bottest/common/getAllProjects.js
+++ bottest/common/getAllProjects.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 //notion api
 const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID
 
 //получить id всех проектов
 module.exports = async function getAllProjects() {
