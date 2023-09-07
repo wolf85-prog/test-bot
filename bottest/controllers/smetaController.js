56676e3d16fbc7d2874964e0c5091e30080853d4
seregey gornih <you@example.com>
2023-09-07 12:30:51 +0300

    fix

--- bottest/controllers/smetaController.js
+++ bottest/controllers/smetaController.js
@@ -9,21 +9,21 @@ async function getSmeta() {
             database_id: databaseSmetaId
         });
 
-        const responseResults = response.results.map((page) => {
-            return {
-               id: page.id,
-               title: page.properties.Name.title[0]?.plain_text,
-               time: page.properties["Дата"].date,
-               time_start: page.properties["Дата"].date.start,
-               time_created: page.created_time,
-               geo: '', //page.properties.Address.rollup.array,
-               teh: page.properties["Тех. задание"].rich_text,
-               status_id: page.properties["Статус проекта"].select,
-               manager: page.properties["Менеджер"].relation[0]?.id,
-               company: page.properties["Компания"].relation[0]?.id,
-               worklist:'',
-            };
-        });
+        // const responseResults = response.results.map((page) => {
+        //     return {
+        //        id: page.id,
+        //        title: page.properties.Name.title[0]?.plain_text,
+        //        time: page.properties["Дата"].date,
+        //        time_start: page.properties["Дата"].date.start,
+        //        time_created: page.created_time,
+        //        geo: '', //page.properties.Address.rollup.array,
+        //        teh: page.properties["Тех. задание"].rich_text,
+        //        status_id: page.properties["Статус проекта"].select,
+        //        manager: page.properties["Менеджер"].relation[0]?.id,
+        //        company: page.properties["Компания"].relation[0]?.id,
+        //        worklist:'',
+        //     };
+        // });
 
         return response;
     } catch (error) {
