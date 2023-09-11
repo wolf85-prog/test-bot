a1b7f4987a256997ed807198c059652dd7c8dbcb
Sergey <seregaland@yandex.ru>
2023-09-11 15:52:39 +0300

    fix

--- bottest.js
+++ bottest.js
@@ -116,7 +116,7 @@ bottest.on('message', async (msg) => {
             await addDate(block3.results[1].id);
         }
 
-        if (text === '/getSmeta') {
+        if (text.startsWith('/getSmeta')) {
             const projectId = text.split(' ');
             //найти смету по свойству Проект
             const smetaId = await getSmeta(projectId[1])
