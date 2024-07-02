b5b77af27fc2a3f962ef2625fd0e044677d37a91
Sergey <seregaland@yandex.ru>
2024-07-02 10:48:33 +0300

    fix

--- bottest/controllers/distributionController.js
+++ bottest/controllers/distributionController.js
@@ -188,19 +188,19 @@ class DistributionController {
                                 )
                             }                    
                         } else {
-                            if (type === 1) {
+                            if (type === '1') {
                                 url_send_photo = `https://api.telegram.org/bot${token}/sendPhoto?chat_id=${user}&photo=${image}&reply_markup=${editButton ? keyboard : keyboard2}`
                                 console.log("url_send_photo2: ", url_send_photo)
                             } 
-                            else if (type === 2) { 
+                            else if (type === '2') { 
                                 url_send_photo = `https://api.telegram.org/bot${token}/sendDocument?chat_id=${user}&document=${image}&reply_markup=${editButton ? keyboard : keyboard2}`
                                 console.log("url_send_document2: ", url_send_photo)
                             }
-                            else if (type === 3) { 
+                            else if (type === '3') { 
                                 url_send_photo = `https://api.telegram.org/bot${token}/sendAudio?chat_id=${user}&audio=${image}&reply_markup=${editButton ? keyboard : keyboard2}`
                                 console.log("url_send_audio2: ", url_send_photo)
                             }
-                            else if (type === 4) { 
+                            else if (type === '4') { 
                                 url_send_photo = `https://api.telegram.org/bot${token}/sendVideo?chat_id=${user}&video=${image}&reply_markup=${editButton ? keyboard : keyboard2}`
                                 console.log("url_send_video2: ", url_send_photo)
                             }
