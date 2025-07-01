ecb9b0b6efd7273ede97a98426a69ef7578bc945
Sergey <seregaland@yandex.ru>
2025-07-01 23:15:54 +0300

    fix

--- bottest/controllers/distributionController.js
+++ bottest/controllers/distributionController.js
@@ -113,7 +113,7 @@ class DistributionController {
                             keyboard = JSON.stringify({
                                 inline_keyboard: [
                                     [
-                                        {"text": textButton, web_app: {url: target}}, 
+                                        {"text": textButton, url: 'https://vk.com/uley.team?from=groups&w=wall-212603682_240'}, 
                                     ],
                                 ]
                             });
